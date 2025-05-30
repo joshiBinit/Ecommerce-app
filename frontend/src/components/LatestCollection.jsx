33ea254d200bd663e94f95f6f306c6eae772aea1
Binit Joshi <binitjoshi4554@gmail.com>
2025-05-31 04:45:43 +0545

    frontend started

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useContext } from "react";
+import Title from "./Title";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          lkajsdoiasjcoinasoicnaisjncioasnciopnhasisohnfoiasnclknasi0ofchnaiosnmpiashncionas
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollection;
