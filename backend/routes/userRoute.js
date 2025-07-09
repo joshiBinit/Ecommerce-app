0092682d5ac2072d5100c4e4be2ea7adfac31baf
Binit Joshi <binitjoshi4554@gmail.com>
2025-07-09 21:56:25 +0545

    Admin Page initiated

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,4 +3,14 @@ import {
   loginUser,
   registerUser,
   adminLogin,
-} from "../controllers/userController";
+} from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+
+userRouter.post("/login", loginUser);
+
+userRouter.post("/admin", adminLogin);
+
+export default userRouter;
