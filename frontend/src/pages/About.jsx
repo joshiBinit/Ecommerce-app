7532c7f622c63aa32de3987cf06e5fdf0d6b6ab9
Binit Joshi <binitjoshi4554@gmail.com>
2025-11-18 15:35:36 +0545

    renamed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
-import NewsLetterBox from "../components/NewsLetterBox";
+import NewsLetterBox from "../components/test";
 
 const About = () => {
   return (
